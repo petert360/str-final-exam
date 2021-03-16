9ea2841b48cdb9db40e471644e69a7f2947f6a5e
petert360 <peter.t360@hotmail.com>
2021-03-16 18:22:56 +0100

    SorterPipe elkészítése

--- src/app/pipe/sorter.pipe.ts
+++ src/app/pipe/sorter.pipe.ts
@@ -5,8 +5,20 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class SorterPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: any[] | null, key: string): any[] | null {
+    if (!Array.isArray(value) || !key) {
+      return value;
+    }
+
+    return value.sort((a, b) => {
+      if (typeof a[key] === 'number' && b[key] === 'number') {
+        return a[key] - b[key];
+      }
+
+      return ('' + a[key]).toLowerCase().localeCompare(('' + b[key]).toLowerCase())
+    });
+
   }
+
 
 }
